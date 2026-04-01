1bddf1e1def01d2c27e1efb2801bde28b87c3764
guillemverdaguer <gverdabo7@alumnes.ub.edu>
2026-04-01 17:22:46 +0200

    si

--- src/prog2/model/TascaManteniment.java
+++ src/prog2/model/TascaManteniment.java
@@ -1,6 +1,8 @@
 package prog2.model;
 
 public class TascaManteniment implements InTascaManteniment {
+
+
     public static enum TipusTascaManteniment{
         Reparacio,
         Neteja,
@@ -9,13 +11,74 @@ public class TascaManteniment implements InTascaManteniment {
     };
     private int numero;
     private Allotjament allotjament;
+    private TipusTascaManteniment tipus;
     private String data;
     private int dies;
 
-    public TascaManteniment(int numero, Allotjament allotjament, TipusTascaManteniment, String data, int dies) {
+    public TascaManteniment(int numero, Allotjament allotjament, TipusTascaManteniment tipus, String data, int dies) {
         this.numero = numero;
         this.allotjament = allotjament;
+        this.tipus = tipus;
         this.data = data;
-        this
+        this.dies = dies;
     }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Allotjament getAllotjament() {
+        return allotjament;
+    }
+
+    public void setAllotjament(Allotjament allotjament) {
+        this.allotjament = allotjament;
+    }
+
+    public TipusTascaManteniment getTipus() {
+        return tipus;
+    }
+
+    public void setTipus(TipusTascaManteniment tipus) {
+        this.tipus = tipus;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getDies() {
+        return dies;
+    }
+
+    public void setDies(int dies) {
+        this.dies = dies;
+    }
+
+    public String getIluminacioAllotjament() {
+         if(tipus == TipusTascaManteniment.Reparacio) {
+             return "50%";
+         }
+         else if(tipus == TipusTascaManteniment.Neteja) {
+             return "100%";
+         }
+         else if(tipus  == TipusTascaManteniment.RevisioTecnica) {
+             return "50%";
+         }
+         else if(tipus == TipusTascaManteniment.Desinfeccio) {
+             return "0%";
+         }
+
+        }
+    }
+
+
 }
